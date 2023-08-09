665f70356b9ee689b8d5be5d88938e1a0bb7fb45
arsham-aazami <arsham20302040aazami@gmail.com>
2023-08-09 12:31:49 +0330

    making navigatin logic

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:store/consts.dart';
+import 'package:store/routes/profile.dart';
+import 'package:store/routes/saved.dart';
+import 'package:store/routes/search.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,34 +14,39 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int currentIndex = 0;
+  List<Widget> bottomTap = [
+    SearchBox(),
+    Text("Home Page"),
+    SavedPage(),
+    Profile()
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        body: bottomTap.elementAt(currentIndex),
         bottomNavigationBar: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Consts.customizedBlue,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: GNav(
-            rippleColor: Consts.customizedBlue,
-            activeColor: Consts.customizedBlue,
-            tabBackgroundColor: Consts.colorStyleThree,
-            tabBorderRadius: 15,
-            iconSize: 30,
-            gap: 2,
-            tabs: const [
-              GButton(icon: Icons.search),
-              GButton(icon: Icons.home),
-              GButton(icon: Icons.bookmark),
-              GButton(icon: Icons.person),
-            ],
-            selectedIndex: currentIndex,
-            onTabChange: (index) => setState(() => currentIndex = index)
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Consts.customizedBlue,
+              borderRadius: BorderRadius.circular(15),
             ),
-            
-      ),
-    ));
+            child: GNav(
+                rippleColor: Consts.customizedBlue,
+                activeColor: Consts.customizedBlue,
+                tabBackgroundColor: Consts.colorStyleThree,
+                tabBorderRadius: 15,
+                iconSize: 30,
+                
+                tabs: const [
+                  GButton(icon: Icons.search),
+                  GButton(icon: Icons.home),
+                  GButton(icon: Icons.bookmark),
+                  GButton(icon: Icons.person),
+                ],
+                selectedIndex: currentIndex,
+                onTabChange: (index) => setState(() => currentIndex = index)),
+          ),
+        ));
   }
 }
---
+++ lib/routes/search.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SearchBox extends StatelessWidget {
+  const SearchBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text("Search Page"),
+    );
+  }
+}
